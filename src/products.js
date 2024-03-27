ef5c14039e7bb9a0e7a7f5ad65c73119e4f4ba66
Newellskk <Newellsikuku@gmail.com>
2024-03-27 17:30:21 +0300

    added services page

---
+++ src/products.js
@@ -0,0 +1,23 @@
+export const products=[
+    {
+        name:'Socks',
+        img:'https://media.istockphoto.com/id/531137761/photo/socks.jpg?s=612x612&w=0&k=20&c=3S4hI_XAeYRhQZ9fto5QlOmFdEHysjCNCps8ZRHNeQ8=',
+        price:40,
+    },
+    {
+        name:'T-shirt',
+        img:'https://us.sunspel.com/cdn/shop/files/mtsh0001-bkaa-1.jpg?v=1690980330&width=1100',
+        price:50,
+    },
+    {
+        name:'Duvet',
+        img:'https://media.istockphoto.com/id/536089355/photo/blue-folded-duvet.jpg?s=612x612&w=0&k=20&c=wyLuZBSIltttAJHb-J2nWRlYYDGWqFraHUJjQ2ORjXY=',
+        price:60,
+    },
+    {
+        name:'Trousers',
+        img:'https://img.freepik.com/premium-photo/brown-male-cargo-pants-trousers-isolated-white-background_259229-952.jpg?size=626&ext=jpg',
+        price:10,
+    },
+    
+]
